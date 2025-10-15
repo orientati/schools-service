cab619f7afae0e4464e802a436eb18f839d2cdcc
Federico Zorgnotto <fede.zorgnotto@gmail.com>
2025-10-15 13:39:16 +0200

    modificato template in schools

--- app/main.py
+++ app/main.py
@@ -23,15 +23,19 @@ sentry_sdk.init(
 
 logger = None
 
+
 # RabbitMQ Broker
 async def callback(message):
     async with message.process():
-        print(f"Received message from exchange '{message.exchange}' with routing key '{message.routing_key}': {message.body.decode()}")
+        print(
+            f"Received message from exchange '{message.exchange}' with routing key '{message.routing_key}': {message.body.decode()}")
+
 
 exchanges = {
     "users": callback,
     "banana": callback
 }
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -42,7 +46,7 @@ async def lifespan(app: FastAPI):
     # Avvia il broker asincrono all'avvio dell'app
     broker_instance = broker.AsyncBrokerSingleton()
     connected = await broker_instance.connect()
-    if(not connected):
+    if (not connected):
         logger.error("Could not connect to RabbitMQ. Exiting...")
         sys.exit(1)
         return
@@ -57,6 +61,7 @@ async def lifespan(app: FastAPI):
     logger.info(f"Shutting down {settings.SERVICE_NAME}...")
     await broker_instance.close()
     logger.info("RabbitMQ connection closed.")
+
 
 docs_url = None if settings.ENVIRONMENT == "production" else "/docs"
 redoc_url = None if settings.ENVIRONMENT == "production" else "/redoc"
@@ -80,6 +85,7 @@ current_router.include_router(
 )
 
 app.include_router(current_router, prefix=settings.API_PREFIX)
+
 
 @app.get("/health", tags=["health"])
 def health():
